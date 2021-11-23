81fd566fdaa9a34a1ce0cdab3ac37d3d0266d469
ze-yter <zeyterforurfu@gmail.com>
2021-11-23 18:28:58 +0500

    change input in filter

---
+++ filter_with_filename.py
@@ -0,0 +1,32 @@
+from PIL import Image
+import numpy as np
+
+
+def MakeColor(newMatrix, matrix, size, i, j):
+    for x in range(i, i + size):
+        for y in range(j, j + size):
+            for z in range(3):
+                matrix[x][y][z] = newMatrix
+
+
+def CreateMosaicIMG(img, size, grad):
+    listImg = np.array(Image.open(img)).astype(int)
+    limit = 255 // grad
+    lenImg = len(listImg)
+    height = len(listImg[0])
+    i = 0
+
+    while i < lenImg:
+        j = 0
+        while j < height:
+            part = listImg[i: size + i, j: size + j]
+            sum = np.sum(part)
+            avg = int(sum // (size ** 2))
+            MakeColor(int(avg // limit) * limit / 3, listImg, size, i, j)
+            j += size
+        i += size
+
+    return Image.fromarray(np.uint8(listImg))
+
+
+CreateMosaicIMG('img2.jpg', 10, 50).save('res.jpg')
